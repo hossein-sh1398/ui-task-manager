d7a42bb6f7eed2b632d7f6b00a4baa64787e23d2
Hossein_shirinegad <hosseinshirinegad.66@gmail.com>
2025-03-27 17:45:07 +0330

    edit App.vue component

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class=" container-fluid">
+        <Header />
+    </div>
+    <div class="container mt-3">
+        <router-view></router-view>
+    </div>
+</template>
+
+
+<script setup>
+import Header from './components/Header.vue';
+
+
+</script>
+
+
+<style scoped></style>
